f2b63face5734564b8572977028d212534f32922
AdrianMReds <adrianmontemayor.r@gmail.com>
2023-11-26 21:26:09 -0600

    Backend mongoose connection

--- controllers/quinielaController.js
+++ controllers/quinielaController.js
@@ -1,8 +1,24 @@
-//@desc Registrar usuario
-//@route POST /api/usuarios
-//@access Public
-const createQuiniela = (req, res) => {
-  res.status(200).json({ message: "Quiniela creada" });
-};
+const asyncHandler = require("express-async-handler");
 
-module.exports = { createQuiniela };
+//@desc Mostrar quinielas
+//@route GET /api/quinielas
+//@access Private
+const getQuinielas = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Todas las quinielas" });
+});
+
+//@desc Crear quiniela
+//@route POST /api/quinielas
+//@access Public
+const createQuiniela = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Quiniela creada" });
+});
+
+//@desc Actualizar quiniela
+//@route PUT /api/quinielas
+//@access Private
+const updateQuiniela = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Quiniela actualizada" });
+});
+
+module.exports = { getQuinielas, createQuiniela, updateQuiniela };
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 app.use(cors());
@@ -12,6 +17,8 @@ app.use(express.urlencoded());
 
 app.use("/api/usuarios", require("./routes/userRoutes"));
 app.use("/api/quinielas", require("./routes/quinielaRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`App is listening to port: ${port}`);
